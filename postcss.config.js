0c51df15d452ab380e0de8cb531d0598986405cb
ever cz <czever2015@gmail.com>
2019-11-04 07:42:54 +0800

    -m初始化

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+/**
+ * Created by ever on 2019/11/3.
+ */
+module.exports = ({file, options, env}) => ({
+    parser: file.extname === '.sss' ? 'sugarss' : false,
+    plugins: {
+        'postcss-import': {root: file.dirname},
+        'postcss-preset-env': options['postcss-preset-env'] ? options['postcss-preset-env'] : false,
+        'cssnano': env === 'production' ? options.cssnano : false
+    }
+})
